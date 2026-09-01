53d81590cb9d6eea6c739eab20a7b00b374bba8b
Achal <achal@example.com>
2026-09-01 13:21:55 +0530

    Add MainActivity.kt and MainApplication.kt for com.soulrise.panchang package

---
+++ android/app/src/main/java/com/soulrise/panchang/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.soulrise.panchang
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+import expo.modules.ReactActivityDelegateWrapper
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String = "main"
+
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      ReactActivityDelegateWrapper(this, fabricEnabled, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
+}
